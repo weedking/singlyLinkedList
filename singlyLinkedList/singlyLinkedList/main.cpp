a6a51b7701ac18a4a18dc618d83e7997385bceb8
weedking <260815998@qq.com>
2016-07-20 14:22:28 +0800

    V0.1

---
+++ singlyLinkedList/singlyLinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  singlyLinkedList
+//
+//  Created by 李海明 on 16/7/20.
+//  Copyright © 2016年 李海明. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...新的东西
+    std::cout << "Hello, World!\n";
+    return 0;
+}
